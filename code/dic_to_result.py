8d63e82b85d5f482a5978afe822787b8b755cd7b
paulv <paulvorderbruegge@proton.me>
2025-04-26 01:03:20 +0200

    Fixed filter_hotels

--- code/dic_to_result.py
+++ code/dic_to_result.py
@@ -1,37 +1,41 @@
 
 import json
 
-def filter_hotels(hotels: dict[str, dict[str, object]], hard_list: list[(str, object)]):
+def filter_hotels(hotels: list[dict[str, object]], hard_list: list[(str, object)]):
     """
     Filters the hotels based on the hard_list
 
     Args:
-        hotels (dict): The hotels to be filtered.
+        hotels (list): The hotels to be filtered.
         hard_list (list): The specified hard filters.
         ...
 
     Returns:
-        dict: The filtered hotel dics.
+        list: The filtered hotel list.
     """
-    keys_to_remove = []
-    for hotel_name, hotel_dict in hotels.items():
+
+    hotels_to_remove = []
+
+    for hotel_dict in hotels:
         for (filter_attribute, filter_value) in hard_list:
             if (hotel_dict.get(filter_attribute) != filter_value):
-                keys_to_remove.append(hotel_name)
+                hotels_to_remove.append(hotel_dict)
                 break
-    for key in keys_to_remove:
-        del hotels[key]
+
+    for hotel in hotels_to_remove:
+        hotels.remove(hotel)
+
     return hotels
 
 
 
-def rank_hotels(client, hotels, filtered_json_data):
+
+def rank_hotels(hotels: list[dict[str, object]], soft_list: list[(str, object)]):
     """
-    Ranks the hotels based on ...
+    Ranks the hotels based on the soft_list
 
     Args:
-    ...
-        hotels (list): The list of hotels to rank.
+        hotels (list): The hotels to be ranked.
         ....
 
     Returns:
