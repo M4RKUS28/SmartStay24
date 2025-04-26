912b497ef7c5c561460635be301c8295a9df261b
Luca Bozzetti <luca.bozzetti8@gmail.com>
2025-04-26 11:12:28 +0200

    documentation

--- code/utils.py
+++ code/utils.py
@@ -1,3 +1,37 @@
+"""
+!!!Types for the hotels:
+
+A) What we get from Check24
+dict[str, dict[str, object]]
+Ein dictionary, welches name des Hotels auf ein weiteres dict mapped, welches feature name auf feature value mapped.
+Beispiel: {
+    "hotel1": {
+        "bathrooms": 4
+        }
+}
+
+B) What we get from ChatGPT
+dict[str, object] with the following keys:
+    status: str,
+    features: Dict[str, dict[str, object]]
+
+features is a dict with the following keys:
+    value: object
+    importance: int
+
+Example:
+    {
+    "status": "success",
+    "features":
+        {
+            'rating':
+                {'value': '>9.3', 'importance': 100},
+            'price':
+                {'value': '<40', 'importance': 70}
+        }
+    }
+"""
+
 def dict_to_list(dict_input: dict[str, (str, int)]):
     """
     Filters the JSON data based on ...
