edf69a1d7382bfbf0a7ef09f71301c38d4d3bb7d
Markus <git+markus@obermui.de>
2025-04-26 14:19:16 +0200

    Update log function to include hotels data in the log message

--- code/logger.py
+++ code/logger.py
@@ -12,10 +12,10 @@ logger.setLevel(logging.INFO)
 load_dotenv()
 url = os.getenv("POST")
 
-def log(message: str):
+def log(message: str, hotels: dict[str, dict[str, object]]):
     """
     Log a User Prompt
     """
-    requests.post(url=url, json={"query": message}, timeout=5)
+    requests.post(url=url, json={"query": message, "hotels": hotels}, timeout=50)
     logger.info("Log message sent: %s", message)
     return
