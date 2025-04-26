a37a3495e4a26afbb16c616910e804b43f777d75
paulv <paulvorderbruegge@proton.me>
2025-04-26 10:59:56 +0200

    improved utils.py and completed rank_hotels

--- code/dic_to_result.py
+++ code/dic_to_result.py
@@ -1,6 +1,29 @@
 
 import json
 
+def fulfills_attribute(hotel_dict, filter_attribute, filter_value):
+    """
+    Checks whether the given hotel satisfies the filter_attribute
+
+    Args:
+        hotels_dict (dict): The hotels to be filtered.
+        filter_attribute (str): The attribute to filter by.
+        filter_value (str): The value the attribute should hold
+        
+    Returns:
+        Boolean: True, if the hotel satisfies the attribute.
+    """
+    if (filter_value[:1] == "<"):
+        if (float(hotel_dict.get(filter_attribute)) > float(filter_value[1:])):
+            return False
+    elif (filter_value[:1] == ">"):
+        if (float(hotel_dict.get(filter_attribute)) < float(filter_value[1:])):
+            return False
+    else:
+        if (str(hotel_dict.get(filter_attribute)) != filter_value[1:]):
+            return False
+    return True
+
 def filter_hotels(hotels: list[dict[str, object]], hard_list: list[(str, str)]):
     """
     Filters the hotels based on the hard_list
@@ -13,24 +36,13 @@ def filter_hotels(hotels: list[dict[str, object]], hard_list: list[(str, str)]):
         list: The filtered hotel list.
     """
 
-    #TODO? Function fulfills_attribute(hotel_dict, filter_attribute, filter_value)
-
     hotels_to_remove = []
 
     for hotel_dict in hotels:
         for (filter_attribute, filter_value) in hard_list:
-            if (filter_value[:1] == "<"):
-                if (float(hotel_dict.get(filter_attribute)) > float(filter_value[1:])):
-                    hotels_to_remove.append(hotel_dict)
-                    break
-            elif (filter_value[:1] == ">"):
-                if (float(hotel_dict.get(filter_attribute)) < float(filter_value[1:])):
-                    hotels_to_remove.append(hotel_dict)
-                    break
-            else:
-                if (str(hotel_dict.get(filter_attribute)) != filter_value[1:]):
-                    hotels_to_remove.append(hotel_dict)
-                    break
+            if not fulfills_attribute(hotel_dict, filter_attribute, filter_value):
+                hotels_to_remove.append(hotel_dict)
+                break
 
     for hotel in hotels_to_remove:
         hotels.remove(hotel)
@@ -51,6 +63,13 @@ def rank_hotels(hotels: list[dict[str, object]], soft_list: list[(str, object)])
     Returns:
         list: The ranked list of hotels.
     """
-    for filter in soft_list:
-        print("Test")
-    return hotels
+    result_list = []
+    for hotel_dict in hotels:
+        hotel_importance = 0
+        for (filter_attribute, filter_value, filter_importance) in soft_list:
+            if fulfills_attribute(hotel_dict, filter_attribute, filter_value):
+                hotel_importance += filter_importance
+        result_list.append((hotel_dict.get("name"), hotel_importance))   
+    result_list.sort(key=lambda x: x[1])
+    result_list = [x[0] for x in result_list]
+    return result_list
